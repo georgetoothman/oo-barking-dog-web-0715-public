a69d380435a8c5ee98b2d325f57061453a234888
George Toothman <george.toothman@gmail.com>
2015-08-04 16:21:49 -0400

    add dog getter and puts a woof command

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Dog
+  # def initialize(name, bark)
+
+  # end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+end
+
+# fido = Dog.new
+# fido.name = "Fido"
+# fido.name
+# fido.bark
